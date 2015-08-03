25af76c909841a69e8e7cf78e2e70059d07cad2b
ASteinheiser <asteinhe@asu.edu>
2015-08-02 17:18:04 -0700

    added react-router, added a not-found-page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,71 @@
 import React from 'react';
 import Radium from 'radium';
+import Router from 'react-router';
 
 import LoginPage from './components/login/login-page';
+import SignupPage from './components/signup/signup-page';
+import NotFoundPage from './components/notfound/not-found-page';
 
-React.render(<LoginPage />, document.getElementById('app'));
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+var App = React.createClass({
+  render () {
+    return (
+      <div>
+        <RouteHandler />
+      </div>
+    )
+  }
+});
+
+var SignupRoute = React.createClass({
+  componentDidMount: function () {
+    var id = this.props.params.id;
+    fetchMessage(id, function (err, message) {
+      this.setState({ message: message });
+    })
+  },
+
+  render () {
+    return <SignupPage />;
+  }
+});
+
+var LoginRoute = React.createClass({
+  componentDidMount: function () {
+    var id = this.props.params.id;
+    fetchMessage(id, function (err, message) {
+      this.setState({ message: message });
+    })
+  },
+
+  render () {
+    return <LoginPage />;
+  }
+});
+
+var NotFoundRoute = React.createClass({
+  componentDidMount: function () {
+    var id = this.props.params.id;
+  },
+
+  render () {
+    return <NotFoundPage />;
+  }
+});
+
+
+var routes = (
+  <Route path="/" handler={App}>
+    <Route handler={LoginRoute}/>
+    <Route handler={NotFoundRoute}/>
+    <Route name="signup" handler={SignupRoute}/>
+  </Route>
+);
+
+Router.run(routes, Router.HashLocation, function(Root) {
+  React.render(<Root/>, document.getElementById('root'));
+});
+
+// React.render(<LoginPage />, document.getElementById('app'));
---
+++ src/components/notfound/not-found-page.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Radium from 'radium';
+
+var NotFoundPage = React.createClass({
+  render: function() {
+    return (
+      <div style={[styles.NotFoundBackground]}>
+        <div style={[styles.NotFoundText]}>
+          <font>Page Not Found</font>
+        </div>
+      </div>
+    );
+  }
+});
+
+NotFoundPage = Radium(NotFoundPage);
+
+var styles = {
+  NotFoundBackground: {
+    color: 'white',
+    backgroundSize: 'cover',
+    display: 'flex',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  NotFoundText: {
+    color: 'rgb(75,75,75)',
+    fontFamily: 'sans-serif',
+    fontWeight: 'lighter',
+    fontSize: '50px',
+    margin: 'auto auto 418px auto',
+  }
+}
+
+export default NotFoundPage;
