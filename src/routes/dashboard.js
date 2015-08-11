a92103c84f131faec40eedd19702b29ce5530bb4
ASteinheiser <asteinhe@asu.edu>
2015-08-10 18:03:07 -0700

    fixed fetchmessage error

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -2,12 +2,6 @@ import React from 'react';
 import DashboardPage from '../components/dashboard/dashboard-page';
 
 var DashboardRoute = React.createClass({
-  componentDidMount: function () {
-    var id = this.props.params.id;
-    fetchMessage(id, function (err, message) {
-      this.setState({ message: message });
-    })
-  },
   render () {
     return <DashboardPage />;
   }
--- src/routes/new-profile.js
+++ src/routes/new-profile.js
@@ -2,12 +2,6 @@ import React from 'react';
 import NewProfile from '../components/newProfile/new-profile-page';
 
 var NewProfileRoute = React.createClass({
-  componentDidMount: function () {
-    var id = this.props.params.id;
-    fetchMessage(id, function (err, message) {
-      this.setState({ message: message });
-    })
-  },
   render () {
     return <NewProfile />;
   }
